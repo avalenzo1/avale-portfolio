8ac0d69aae5573d273d50cdb96cf4ed20a2047ff
Anthony <anthonyvalenzo4@gmail.com>
2024-11-29 14:45:35 -0600

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+  return (
+    <>
+      <div className="hero">
+        <h1>Hi, I'm Anthony 👋</h1>
+        <p>I'm a aspiring Software Developer based in Chicago, Illinois.</p>
+      </div>
+    </>
+  )
+}
+
+export default App
